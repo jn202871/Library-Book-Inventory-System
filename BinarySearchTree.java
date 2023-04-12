db124089c1edeae31f9ec6310514c76753682167
Jay Nash <jn202871@gmail.com>
2023-04-12 10:21:47 -0400

    init

---
+++ BinarySearchTree.java
@@ -0,0 +1,120 @@
+public class BinarySearchTree{
+    private LibNode root;
+
+    public BinarySearchTree(){
+        root = null;
+    }
+
+    public boolean isEmptyTree(){
+        return root == null;
+    }
+
+    public LibNode search(int key){
+        return searchr(root,key);
+    }
+
+    private LibNode searchr(LibNode node, int key){
+        if (node==null){
+            return null;
+        } else if (key == node.getKey()){
+            return node;
+        } else if (key < node.getKey()){
+            return searchr(node.getLeft(),key);
+        } else {
+            return searchr(node.getRight(),key);
+        }
+
+    }
+
+    public void insert(LibNode x){
+        LibNode temp = root;
+        LibNode parent = null;
+
+        while (temp != null){
+            parent=temp;
+            if (x.getKey()<temp.getKey()){
+                temp = temp.getLeft();
+            } else {
+                temp = temp.getRight();
+            }
+        }
+        if (parent==null){
+            root = x;
+        } else if (x.getKey() < parent.getKey()){
+            parent.setLeft(x);
+        } else {
+            parent.setRight(x);
+        }
+    }
+
+    public void printTree() {
+	printTree2(root);
+	System.out.println();
+    }
+
+    private void printTree2(LibNode tree){
+	if (tree != null) {
+	    System.out.print(tree.getKey() + " ");
+            if (tree.getLeft() != null)
+	        System.out.print("Left: " + tree.getLeft().getKey() + " ");
+            else
+                System.out.print("Left: null ");
+            if (tree.getRight() != null)
+	        System.out.println("Right: " + tree.getRight().getKey() + " ");
+            else
+                System.out.println("Right: null ");
+	    printTree2(tree.getLeft());
+	    printTree2(tree.getRight());
+	}
+    }
+
+    public void delete(int key){
+        root = deleter(root,key);
+    }
+
+    private LibNode deleter(LibNode node, int key){
+        if (node == null) {
+            return null;
+        }
+
+        if (key < node.getKey()) {
+            node.setLeft(deleter(node.getLeft(), key));
+        } else if (key > node.getKey()) {
+            node.setRight(deleter(node.getRight(), key));
+        } else {
+            if (node.getLeft() == null) {
+                return node.getRight();
+            } else if (node.getRight() == null) {
+                return node.getLeft();
+            } else {
+            	LibNode temp = node;
+            	node = minKey(node.getRight()); // Find Inorder Successor
+            	temp.setRight(deleter(temp.getRight(), node.getKey()));
+            	node.setRight(temp.getRight());
+            	node.setLeft(temp.getLeft());
+            }
+        }
+
+        return node;
+    }
+
+    private LibNode minKey(LibNode x){
+        while (x.getLeft() != null){
+            x = x.getLeft();
+        }
+        return x;
+    }
+    
+    public void traverse(){
+        traverser(root);
+        System.out.println();
+    }
+
+    private void traverser(LibNode root){
+        if (root != null){
+            traverser(root.getLeft());
+            System.out.println(root.getName());
+            traverser(root.getRight());
+        }
+    }
+}
---
+++ LibNode.java
@@ -0,0 +1,87 @@
+// Node with 2 child nodes
+public class LibNode {
+	
+	// Node information
+	private LibNode right;
+    private LibNode left;
+	private String name;
+	private String author;
+	private boolean checkedOut;
+	private int CRN;
+	
+	// Creation Method
+	public LibNode(String name, String author, int CRN) {
+		this.name = name;
+		this.CRN = CRN;
+		this.right = null;
+        this.left = null;
+		this.author = author;
+		this.checkedOut = false;
+	}
+	
+	// Returns Name
+	public String getName(){
+		return name;
+	}
+		
+	// Returns CRN
+	public int getCRN(){
+		return CRN;
+	}
+	
+	// Returns CRN
+	public int getKey(){
+		return CRN;
+	}
+	
+	// Sets name of node to argument
+	public void setName(String name){
+		this.name = name;
+	}
+	
+	// Sets SSN of node to argument
+	public void setCRN(int CRN){
+		this.CRN = CRN;
+	}
+	
+	// Sets pointer of node to argument
+	public LibNode setRight(LibNode next){
+		this.right = next;
+		return this.right;
+	}
+
+	public LibNode getRight(){
+		return this.right;
+	}
+
+    // Sets pointer of node to argument
+	public LibNode setLeft(LibNode next){
+		this.left = next;
+		return this.left;
+	}
+
+	public LibNode getLeft(){
+		return this.left;
+	}
+
+	public String getAuthor(){
+		return author;
+	}
+
+	public void setAuthor(String author){
+		this.author = author;
+	}
+
+	public boolean isCheckedOut(){
+		return checkedOut;
+	}
+
+	public void checkIn(){
+		this.checkedOut = false;
+	}
+
+	public void checkOut(){
+		this.checkedOut = true;
+	}
+	
+}
